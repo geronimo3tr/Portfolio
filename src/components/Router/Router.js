12e16ad93a1f3893cf118463ebd496798f845bb7
geronimo3tr <jocelyn.lepagepro@gmail.com>
2024-03-11 17:20:23 +0100

    header css + mainpage

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "../Header/Header";
+import MainPage from "../../pages/MainPage/MainPage";
 
 function RouterPortfolio() {
   return (
     <div>
       <Router>
         <Header />
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
       </Router>
     </div>
   );
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,5 @@
+function MainPage() {
+  return <h1>Présentation</h1>;
+}
+
+export default MainPage;
