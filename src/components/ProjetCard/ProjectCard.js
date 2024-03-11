0984c93cf52bc03a5d881bb69439d1a01df6950f
geronimo3tr <jocelyn.lepagepro@gmail.com>
2024-03-11 18:15:35 +0100

    project card add

---
+++ src/components/ProjetCard/ProjectCard.js
@@ -0,0 +1,19 @@
+function ProjectCard() {
+  return (
+    <>
+      <h1>Projet</h1>
+      <section className="projectcards">
+        <div className="projectcard">
+          <div className="top-card">
+            <h2>project title</h2>
+          </div>
+          <h2>project title</h2>
+          <span>description of the project</span>
+          <div className="experiance-card">SASS</div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default ProjectCard;
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,5 +1,17 @@
+import ProjectCard from "../../components/ProjetCard/ProjectCard";
+
 function MainPage() {
-  return <h1>Présentation</h1>;
+  return (
+    <>
+      <section>
+        <h1>Présentation</h1>
+        <div className="presentation-text">
+          <a>test de presentation</a>
+        </div>
+      </section>
+      <ProjectCard />
+    </>
+  );
 }
 
 export default MainPage;
